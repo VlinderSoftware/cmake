03118f13740956029d3c09e62e9212fff324aa8e
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2015-11-18 16:21:45 -0500

    add socket library, and make sure everyone dumps their binaries in the same place

--- settings.cmake
+++ settings.cmake
@@ -2,3 +2,5 @@ get_filename_component(settings_cmake_dir "${CMAKE_CURRENT_LIST_FILE}" PATH)
 
 include("${settings_cmake_dir}/inc/msvc.cmake")
 include("${settings_cmake_dir}/inc/posix.cmake")
+
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR})
